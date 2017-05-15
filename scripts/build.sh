9466576404a9f5fc2fefd118803ad070daeb5c71
Eyal Soha <eyalsoha@gmail.com>
2017-05-15 15:25:12 +0300

    makefile

--- scripts/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./plot_board.py --out ../out/intermediate ../multivibrator.kicad_pcb && pcb2gcode && ./unify_gcode.py >! ../out/pcb.ngc && gerber2svg -p --out ../out -- ../out/intermediate/multivibrator-F.SilkS.gbr 
--- scripts/plot_board.py
+++ scripts/plot_board.py
@@ -33,7 +33,7 @@ class readable_dir(argparse.Action):
   def __call__(self,parser, namespace, values, option_string=None):
     prospective_dir=values[0]
     if not os.path.isdir(prospective_dir):
-      os.mkdir(prospective_dir)
+      os.makedirs(prospective_dir)
     if os.access(prospective_dir, os.R_OK):
       setattr(namespace,self.dest,prospective_dir)
     else:
